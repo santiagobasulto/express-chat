39817cd3de1bf04dc30cec9bb26349df63fd7437
Santiago Basulto <santiago.basulto@gmail.com>
2015-01-04 14:31:21 -0300

    Removing unnecesary browser-sync in favor of dead-simple nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -29,49 +29,13 @@ gulp.task('js', function () {
     .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('nodemon', function (cb) {
-  var called = false;
-  return nodemon({
+gulp.task('serve', function () {
+  nodemon({script: 'app.js', ext: 'html js', ignore: ['public/']})
+    .on('restart', function () {
 
-    // nodemon our expressjs server
-    script: 'app.js',
-
-    // watch core server file(s) that require server restart on change
-    watch: ['app.js']
-  })
-    .on('start', function onStart() {
-      // ensure start only got called once
-      if (!called) { cb(); }
-      called = true;
     })
-    .on('restart', function onRestart() {
-      // reload connected browsers after a slight delay
-      setTimeout(function reload() {
-        browserSync.reload({
-          stream: false   //
-        });
-      }, BROWSER_SYNC_RELOAD_DELAY);
-    });
-});
+})
 
-gulp.task('browser-sync', ['nodemon'], function () {
-
-  // for more browser-sync config options: http://www.browsersync.io/docs/options/
-  browserSync.init({
-
-    // watch the following files; changes will be injected (css & images) or cause browser to refresh
-    files: ['public/**/*.*'],
-
-    // informs browser-sync to proxy our expressjs app which would run at the following location
-    proxy: 'http://localhost:3000',
-
-    // informs browser-sync to use the following port for the proxied app
-    // notice that the default port is 3000, which would clash with our expressjs
-    port: 5000
-  });
-});
-
-
-gulp.task('default', ['less', 'browser-sync'], function(){
+gulp.task('default', ['less', 'serve'], function(){
     gulp.watch("less/**/*.less", ['less']);
 });
