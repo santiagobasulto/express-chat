31fd98637eb685dff3a6ffca7916d7094e6d1988
Santiago Basulto <santiago.basulto@gmail.com>
2015-01-04 14:30:57 -0300

    Cleaning up app.js

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-// var express = require('express');
-// var server = require('http').Server(app);
-// var io = require('socket.io')(server);
-// var path = require('path');
-
-// var app = express();
-
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'hjs');
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('/', function (req, res) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// // io.on('connection', function (socket) {
-// //   socket.emit('news', { hello: 'world' });
-// //   socket.on('my other event', function (data) {
-// //     console.log(data);
-// //   });
-// // });
-
-// //server.listen(3000);
-// module.exports = app;
 var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
@@ -31,6 +7,7 @@ var path = require('path');
 var routes = require('./routes')
 var realtime = require('./realtime')
 
+// Changed hogan.js delimiters so it won't conflict with Angular
 app.locals.delimiters = '<% %>';
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
